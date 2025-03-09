7614d666f523888ecc0a523d21678d92a5e69832
Milan Sobat <milan.sobat@sykes.com>
2025-03-09 18:06:17 -0230

    Scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { NgbNavModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, NgbNavModule, NgbAccordionModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dukes-removal-services';
+  activeSection = 'home';
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.scrollY + 100; // Adjust for navbar height
+
+    if (scrollPosition < document.getElementById('services')!.offsetTop) {
+      this.activeSection = 'home';
+    } else if (scrollPosition < document.getElementById('contact')!.offsetTop) {
+      this.activeSection = 'services';
+    } else {
+      this.activeSection = 'contact';
+    }
+  }
+
+  scrollToSection(sectionId: string): void {
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+    this.activeSection = sectionId;
+  }
 }
