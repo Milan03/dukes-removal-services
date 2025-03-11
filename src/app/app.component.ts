cfaa0f30e10ac3d6b0d3a2578e4fe1d9b139ac6e
Milan Sobat <milan.sobat@sykes.com>
2025-03-11 11:33:04 -0230

    Work on contact form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,58 @@
 import { Component, HostListener } from '@angular/core';
 import { NgbNavModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, NgbNavModule, NgbAccordionModule],
+  imports: [CommonModule, NgbNavModule, NgbAccordionModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   activeSection = 'home';
+  contactForm: FormGroup;
+
+  services = [
+    "Dump Runs",
+    "Yard Clean Up",
+    "Heavy Lifting Around the House",
+    "Furniture Removal",
+    "Moving Services",
+    "Backyard Pet Clean-up"
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]], // Must be 10-digit number
+      servicesRequested: this.fb.array([]), // Stores selected services
+      description: [''] // Optional
+    });
+  }
+
+  // Method to handle service selection
+  toggleService(service: string, event: any) {
+    const servicesArray = this.contactForm.controls['servicesRequested'].value;
+    if (event.target.checked) {
+      servicesArray.push(service);
+    } else {
+      const index = servicesArray.indexOf(service);
+      if (index !== -1) servicesArray.splice(index, 1);
+    }
+  }
+
+  // Handle form submission
+  submitForm() {
+    if (this.contactForm.valid) {
+      console.log("Form Submitted Successfully:", this.contactForm.value);
+      alert("Thank you! We will get back to you soon.");
+      this.contactForm.reset(); // Reset after submission
+    } else {
+      alert("Please fill in all required fields correctly.");
+    }
+  }
 
   @HostListener('window:scroll', [])
   onScroll(): void {
